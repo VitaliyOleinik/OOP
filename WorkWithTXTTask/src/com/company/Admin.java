082b5d8493b94dcd755cd6a74ecaabba2a6dbaf1
Vitaliy Oleinik <42870696+VitaliyOleinik@users.noreply.github.com>
2021-01-10 23:35:17 +0600

    admin class in process

--- WorkWithTXTTask/src/com/company/Admin.java
+++ WorkWithTXTTask/src/com/company/Admin.java
@@ -1,10 +1,25 @@
 package com.company;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
 class Admin{
-    public static void saveGoodItems(ArrayList<GoodItem> goodItem){
+    ArrayList<GoodItem> goodItems = new ArrayList<>();
+    public ArrayList<GoodItem> getGoodItemList(){
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("goodItems.txt"));
+            String message = "";
+            while((message = br.readLine()) != null){
+               // goodItems.add(message);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public void saveGoodItems(ArrayList<GoodItem> goodItem){
             try{
                 BufferedWriter bw = new BufferedWriter(new FileWriter("goodItems.txt"));
                 for(GoodItem goods : goodItem){
--- WorkWithTXTTask/src/com/company/Main.java
+++ WorkWithTXTTask/src/com/company/Main.java
@@ -8,12 +8,15 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int choose;
         ArrayList<String> goodItems = new ArrayList<>();
-
-        while(true){
-            System.out.println("PRESS [1] ADD GOOD");
-            System.out.println("PRESS [2] LIST GOODS");
-            System.out.println("PRESS [3] DELETE GOOD");
-            System.out.println("PRESS [0] TO EXIT");
+        System.out.println("Who are you? "+"\n"+" [1] Admin " + "\n" + " [2] Client");
+        choose = in.nextInt();
+        if (choose == 1) {
+            while (true) {
+                System.out.println("PRESS [1] ADD GOOD");
+                System.out.println("PRESS [2] LIST GOODS");
+                System.out.println("PRESS [3] DELETE GOOD");
+                System.out.println("PRESS [0] TO EXIT");
+            }
         }
     }
 }
