6f919caac7e8ccb6b56598d9ef8a86956a45e45a
Vitaliy Oleinik <42870696+VitaliyOleinik@users.noreply.github.com>
2021-01-11 22:06:47 +0600

    rewrite admin class + functional in main menu for admin

--- WorkWithTXTTask/src/com/company/Admin.java
+++ WorkWithTXTTask/src/com/company/Admin.java
@@ -10,25 +10,27 @@ class Admin{
     public ArrayList<GoodItem> getGoodItemList(){
         try{
             BufferedReader br = new BufferedReader(new FileReader("goodItems.txt"));
-            String message = "";
-            while((message = br.readLine()) != null){
-               // goodItems.add(message);
+            String nameOfGoodItem = "";
+            while((nameOfGoodItem = br.readLine()) != null){
+                int price = Integer.parseInt(br.readLine()); //Вытягиваем цену сконвертировав в int
+                goodItems.add(new GoodItem(nameOfGoodItem, price));
             }
         }catch (Exception e){
             e.printStackTrace();
         }
-        return null;
+        return goodItems;
+
     }
     public void saveGoodItems(ArrayList<GoodItem> goodItem){
-            try{
-                BufferedWriter bw = new BufferedWriter(new FileWriter("goodItems.txt"));
-                for(GoodItem goods : goodItem){
-                    bw.write(goods.getName() + "\n");
-                    bw.write(goods.getPrice() + "\n");
-                    bw.close();
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter("goodItems.txt"));
+            for(GoodItem goods : goodItem){
+                bw.write(goods.getName() + "\n");
+                bw.write(goods.getPrice() + "\n");
             }
-            }catch (Exception e){
-                e.printStackTrace();
-            }
+            bw.close();
+        }catch (Exception e){
+            e.printStackTrace();
         }
+    }
 }
--- WorkWithTXTTask/src/com/company/Main.java
+++ WorkWithTXTTask/src/com/company/Main.java
@@ -7,15 +7,47 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int choose;
-        ArrayList<String> goodItems = new ArrayList<>();
         System.out.println("Who are you? "+"\n"+" [1] Admin " + "\n" + " [2] Client");
         choose = in.nextInt();
         if (choose == 1) {
+            Admin admin = new Admin();
             while (true) {
                 System.out.println("PRESS [1] ADD GOOD");
                 System.out.println("PRESS [2] LIST GOODS");
                 System.out.println("PRESS [3] DELETE GOOD");
                 System.out.println("PRESS [0] TO EXIT");
+                choose = in.nextInt();
+                if(choose == 1){
+                    System.out.println("Write the name of good");
+                    String name = in.next();
+                    System.out.println("Write the price of this good");
+                    int price = in.nextInt();
+                    // create new object
+                    GoodItem gI = new GoodItem(name, price);
+                    // read list with new object
+                    ArrayList<GoodItem> goodItems = admin.getGoodItemList();
+                    // adding an object to list
+                    goodItems.add(gI);
+                    // write to txt file
+                    admin.saveGoodItems(goodItems);
+                }else if(choose == 2){
+                    ArrayList<GoodItem> goodItems = admin.getGoodItemList();
+                    for (int i = 0; i < goodItems.size(); i++){
+                        System.out.println(goodItems.get(i).toString());
+                    }
+                    System.out.println(goodItems.size());
+                }else if(choose == 3){
+                    ArrayList<GoodItem> goodItems = admin.getGoodItemList();
+                    for (int i = 0; i < goodItems.size(); i++){
+                        System.out.println(goodItems.get(i).toString());
+                    }
+                    System.out.println("Insert index of good item to delete: ");
+                    int index = in.nextInt(); //Вводим порядковый номер игрока из списка от 1 до размера
+                    goodItems.remove(index - 1);
+                    admin.saveGoodItems(goodItems);
+                }else if(choose == 0){
+                    return;
+                }
             }
         }
     }
